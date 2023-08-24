25f234a9377f73f9d5bf26b77e124cdf8ab48c2f
mbrt-yeah <registrations@mbrt.io>
2023-08-24 09:42:44 +0200

    Renders new functions to transform letters to numbers and vice versa exportable

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./arrayfy";
 export * from "./make-string-human-readable";
 export * from "./remove-all-whitespaces-from-string";
+export * from "./transform-letters-to-number";
+export * from "./transform-number-to-letters";
 export * from "./try-catch";
