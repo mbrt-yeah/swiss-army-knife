f94769dfcbfceaef3bf8a2818c6f81c22773b5e5
mbrt-yeah <registrations@mbrt.io>
2023-09-07 15:42:08 +0200

    Adds more complex return object

--- packages/utilities/src/traverse-dir.ts
+++ packages/utilities/src/traverse-dir.ts
@@ -3,7 +3,13 @@
 import { join } from "node:path";
 import { Stats, readdirSync, statSync } from "node:fs";
 
-export function* traverseDir(dirPath: string): Generator<Stats> {
+interface ITraverseDirResult {
+    stats: Stats;
+    path: string;
+    rootPath?: string;
+};
+
+function* traverseDir(dirPath: string): Generator<ITraverseDirResult> {
     const contents = readdirSync(dirPath, {
         withFileTypes: true
     });
@@ -12,9 +18,15 @@ export function* traverseDir(dirPath: string): Generator<Stats> {
         const contentPath = join(dirPath, content.name);
         const contentStats = statSync(contentPath);
 
-        yield contentStats;
+        yield {
+            path: contentPath,
+            rootPath: dirPath,
+            stats: contentStats
+        };
 
         if (content.isDirectory())
             yield* traverseDir(contentPath);
     }
 };
+
+export { ITraverseDirResult, traverseDir };
