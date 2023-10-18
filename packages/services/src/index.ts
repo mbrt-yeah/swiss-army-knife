cb9c9a93f61c008b7d3c9e262403fec45bd1e4be
mbrt-yeah <registrations@mbrt.io>
2023-10-18 18:36:14 +0200

    Exports DirExplorer and FileContentReader services

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./csv-parser/index.js";
 export * from "./csv-serializer/index.js";
+export * from "./dir-explorer/index.js";
+export * from "./file-content-reader/index.js";
 export * from "./file-reader/index.js";
 export * from "./file-writer/index.js";
 export * from "./xml-parser/index.js";
