c66f818ba9febbd12934c118124c724dc3aed3fa
mbrt-yeah <registrations@mbrt.io>
2023-10-25 13:38:41 +0200

    Uses now @swiss-army-knife/build-scripts to create build-specific package.json files

--- packages/services/build-scripts/create-package-json-for-cjs-build.js
+++ packages/services/build-scripts/create-package-json-for-cjs-build.js
@@ -1,9 +1,9 @@
-const path = require("path");
-const createPackageJsonForBuild = require("./create-package-json-for-build");
+const { createPackageJsonForBuild } = require("@swiss-army-knife/build-scripts");
+const { resolve } = require("path");
 
 createPackageJsonForBuild({
-    packageJsonPath: path.resolve(__dirname, "../package.json"),
-    tsConfigPath: path.resolve(__dirname, "../tsconfig.cjs.json"),
+    absoluteModuleBasePath: resolve(__dirname, "../"),
+    relativeTsConfigPath: "./tsconfig.cjs.json",
     packageJsonProps: {
         type: "commonjs"
     }
